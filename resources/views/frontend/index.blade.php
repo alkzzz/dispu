f60647bdc63837c29bd3c807d29fb28585c50119
alkzzz <m.alkaff9@gmail.com>
2023-06-23 09:00:12 +0800

    template depan 6 bidang done

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -28,34 +28,40 @@
             </div>
         </div>
     </div>
-    <div class="row">
+    <div class="row bg-white">
         <div class="container-fluid">
-            <div class="jumbotron my-5">
+            <div class="jumbotron my-5 md-my-2">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-6 mt-5">
-                            <h5 class="fw-semibold" style="color: #FFD42B">Dinas Pekerjaan Umum dan Penataan Ruang Kota
-                                Banjarbaru</h5>
-                            <h2 class="fw-bold" style="line-height: 2.5rem">Selamat Datang di Website Resmi <br>Dinas
-                                Pekerjaan Umum dan Penataan
-                                Ruang
-                                Kota Banjarbaru</h2>
-                            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, ea similique,
-                                veritatis pariatur repellat, vitae expedita nam ut repellendus maxime provident cupiditate
-                                animi voluptate. Qui accusamus fugiat rem. Vero, illo.
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus architecto omnis alias
-                                eos fuga velit nemo neque tempore quibusdam, earum totam fugit atque nulla illo, vitae
-                                laboriosam, officia dolorem eveniet?
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, eveniet ex debitis
-                                quis nulla esse quibusdam, reiciendis quod quas nesciunt doloribus totam dicta tempore
-                                impedit aliquam, expedita sit repellat nemo.
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit beatae ipsum iure nobis
-                                provident, voluptatum consequatur optio ratione iste, iusto pariatur, illo itaque aut non
-                                molestias voluptas neque quod cumque.</p>
-                            <a class="btn btn-primary btn-lg" href="#" role="button">Selengkapnya <i
-                                    class="fa-solid fa-arrow-right-long"></i></a>
+                        <div class="col-md-6 md-mt-2 mt-5 order-2">
+                            <div class="container">
+                                <h5 class="fw-semibold" style="color: #FFD42B">Dinas Pekerjaan Umum dan Penataan Ruang Kota
+                                    Banjarbaru</h5>
+                                <h2 class="fw-bold" style="line-height: 2.5rem">Selamat Datang di Website Resmi <br>Dinas
+                                    Pekerjaan Umum dan Penataan
+                                    Ruang
+                                    Kota Banjarbaru</h2>
+                                <p class="lead" style="text-align: justify">Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. A, ea similique,
+                                    veritatis pariatur repellat, vitae expedita nam ut repellendus maxime provident
+                                    cupiditate
+                                    animi voluptate. Qui accusamus fugiat rem. Vero, illo.
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus architecto omnis
+                                    alias
+                                    eos fuga velit nemo neque tempore quibusdam, earum totam fugit atque nulla illo, vitae
+                                    laboriosam, officia dolorem eveniet?
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, eveniet ex debitis
+                                    quis nulla esse quibusdam, reiciendis quod quas nesciunt doloribus totam dicta tempore
+                                    impedit aliquam, expedita sit repellat nemo.
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit beatae ipsum iure nobis
+                                    provident, voluptatum consequatur optio ratione iste, iusto pariatur, illo itaque aut
+                                    non
+                                    molestias voluptas neque quod cumque.</p>
+                                <a class="btn btn-primary btn-lg" href="#" role="button">Selengkapnya <i
+                                        class="fa-solid fa-arrow-right-long"></i></a>
+                            </div>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-6 order-1 d-flex justify-content-center px-0">
                             <img src="{{ asset('img/Gambar Section 1.png') }}" alt="Jumbotron Image" class="img-fluid">
                         </div>
                     </div>
@@ -65,15 +71,15 @@
     </div>
     <div class="row">
         <div class="container-fluid">
-            <div class="row">
-                <div class="px-0 col-5">
-                    <div style="padding-top:5rem;padding-bottom:5rem">
+            <div class="row bg-white">
+                <div class="px-0 col-12 col-md-5 d-none d-md-block">
+                    <div style="padding-top: 5rem; padding-bottom: 5rem">
                         <img class="img-fluid" src="{{ asset('img/Gambar Section 2.png') }}" alt="Image" />
                     </div>
                 </div>
-                <div class="col-7 bg-warning rounded-3">
-                    <div style="padding-left:2rem;padding-top:5rem">
-                        <h2 class="fw-bold" style="line-height: 2.5rem">Bidang Dinas Pekerjaan Umum dan Penataan Ruang
+                <div class="col-12 col-md-7 bg-warning rounded-3">
+                    <div style="padding-left: 2rem; padding-top: 5rem">
+                        <h2 class="fw-bold mt-4" style="line-height: 2.5rem">Bidang Dinas Pekerjaan Umum dan Penataan Ruang
                             <br>Kota Banjarbaru
                         </h2>
                         <h5 class="mb-4" style="line-height: 2rem">Terdapat 6 Bidang Unit Organisasi dalam Dinas Pekerjaan
@@ -81,55 +87,71 @@
                             Penataan Ruang <br> Kota
                             Banjarbaru
                         </h5>
-                        <div class="row mb-4">
-                            <div class="card me-4" style="width: 15rem;">
-                                <div class="card-body">
-                                    <h5 class="card-title">Special title treatment</h5>
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional
-                                        content.</p>
+                        <div class="row">
+                            <div class="col mb-3">
+                                <div class="card d-flex justify-content-center h-100" style="width: 15rem;">
+                                    <img class="card-img-top mt-3" src="{{ asset('img/icon/icon bidang sekretariat.svg') }}"
+                                        alt="Bidang Sekretariat" style="width: 25%; margin: 0 auto;" />
+                                    <div class="card-body text-center">
+                                        <h5 class="card-title">Bidang Sekretariat</h5>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="card me-4" style="width: 15rem;">
-                                <div class="card-body">
-                                    <h5 class="card-title">Special title treatment</h5>
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional
-                                        content.</p>
+                            <div class="col mb-3">
+                                <div class="card d-flex justify-content-center h-100" style="width: 15rem;">
+                                    <img class="card-img-top mt-3" src="{{ asset('img/icon/icon bidang bina marga.svg') }}"
+                                        alt="Bidang Bina Marga" style="width: 25%; margin: 0 auto;" />
+                                    <div class="card-body text-center">
+                                        <h5 class="card-title">Bidang Bina Marga</h5>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="card" style="width: 15rem;">
-                                <div class="card-body">
-                                    <h5 class="card-title">Special title treatment</h5>
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional
-                                        content.</p>
+                            <div class="col mb-3">
+                                <div class="card d-flex justify-content-center h-100" style="width: 15rem;">
+                                    <img class="card-img-top mt-3" src="{{ asset('img/icon/icon bidang tata ruang.svg') }}"
+                                        alt="Bidang Tata Ruang" style="width: 25%; margin: 0 auto;" />
+                                    <div class="card-body text-center">
+                                        <h5 class="card-title">Bidang Tata Ruang</h5>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="card me-4" style="width: 15rem;">
-                                <div class="card-body">
-                                    <h5 class="card-title">Special title treatment</h5>
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional
-                                        content.</p>
+                            <div class="col mb-3">
+                                <div class="card d-flex justify-content-center h-100" style="width: 15rem;">
+                                    <img class="card-img-top mt-3"
+                                        src="{{ asset('img/icon/icon bidang cipta karya.svg') }}" alt="Bidang Cipta Karya"
+                                        style="width: 25%; margin: 0 auto;" />
+                                    <div class="card-body text-center">
+                                        <h5 class="card-title">Bidang Cipta Karya</h5>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="card me-4" style="width: 15rem;">
-                                <div class="card-body">
-                                    <h5 class="card-title">Special title treatment</h5>
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional
-                                        content.</p>
+                            <div class="col mb-3">
+                                <div class="card d-flex justify-content-center h-100" style="width: 15rem;">
+                                    <img class="card-img-top mt-3"
+                                        src="{{ asset('img/icon/icon bidang pengembangan konstruksi.svg') }}"
+                                        alt="Bidang Pengembangan Konstruksi" style="width: 25%; margin: 0 auto;" />
+                                    <div class="card-body text-center">
+                                        <h5 class="card-title">Bidang Pengembangan Konstruksi</h5>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="card" style="width: 15rem;">
-                                <div class="card-body">
-                                    <h5 class="card-title">Special title treatment</h5>
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional
-                                        content.</p>
+                            <div class="col mb-3">
+                                <div class="card d-flex justify-content-center h-100" style="width: 15rem;">
+                                    <img class="card-img-top mt-3"
+                                        src="{{ asset('img/icon/icon bidang sumber daya air.svg') }}"
+                                        alt="Bidang Sumber Daya Air" style="width: 25%; margin: 0 auto;" />
+                                    <div class="card-body text-center">
+                                        <h5 class="card-title">Bidang Sumber Daya Air</h5>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
     <div class="row">

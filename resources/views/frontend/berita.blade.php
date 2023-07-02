b32c9ff463483380066a32c19398aa6a22286028
alkzzz <m.alkaff9@gmail.com>
2023-07-02 18:25:02 +0800

    add show berita and galery dummy version

--- resources/views/frontend/berita.blade.php
+++ resources/views/frontend/berita.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.frontend.layout')
 
+@section('title', 'Berita')
+
 @section('breadcrumb')
     <div class="py-1" style="background-color:#eeb432;font-size:1.1rem">
         <nav aria-label="breadcrumb" class="container mt-3">
@@ -13,34 +15,36 @@
 
 @section('content')
     <div class="container mt-4">
+        <h2 class="border-bottom border-2 black" style="margin-bottom: 3rem">Berita Dinas Pekerjaan Umum dan Penataan Ruang
+            Kota Banjarbaru</h2>
         <div class="row">
-            <div class="row">
-                <div class="col-8">
-                    <h3>Judul Berita</h3>
-                    <p class="fs-5" style="text-align: justify">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Quod
-                        minima debitis
-                        pariatur
-                        labore praesentium
-                        iusto voluptatibus in sit eius natus alias, soluta voluptate. Reprehenderit soluta voluptas odio,
-                        velit
-                        perferendis numquam! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente nisi
-                        repellat
-                        corrupti, quod facilis harum eum. Commodi saepe consequatur molestiae fugit doloremque asperiores
-                        excepturi libero sapiente aliquam provident. Deserunt, officia!
-                    </p>
-                    <a name="" id="" class="btn btn-primary" href="{{ url('galeri') }}"
-                        role="button">Selengkapnya <i class="fa-solid fa-arrow-right-long"></i></a>
+            <div class="col-8">
+                <h3>Judul Berita</h3>
+                <p class="fs-5" style="text-align: justify">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quod
+                    minima debitis
+                    pariatur
+                    labore praesentium
+                    iusto voluptatibus in sit eius natus alias, soluta voluptate. Reprehenderit soluta voluptas odio,
+                    velit
+                    perferendis numquam! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente nisi
+                    repellat
+                    corrupti, quod facilis harum eum. Commodi saepe consequatur molestiae fugit doloremque asperiores
+                    excepturi libero sapiente aliquam provident. Deserunt, officia!
+                </p>
+                <a name="" id="" class="btn btn-primary" href="{{ url('galeri') }}"
+                    role="button">Selengkapnya <i class="fa-solid fa-arrow-right-long"></i></a>
 
-                </div>
-                <div class="col-4">
-                    <div class="card shadow p-1 mb-3 bg-white rounded">
-                        <img class="img-fluid" src="https://picsum.photos/500?random=1" alt="" srcset="">
-                        <div class="card-img-overlay"></div>
-                    </div>
+            </div>
+            <div class="col-4">
+                <div class="card shadow p-1 mb-3 bg-white rounded">
+                    <img class="img-fluid" src="https://picsum.photos/500?random=1" alt="" srcset="">
+                    <div class="card-img-overlay"></div>
                 </div>
             </div>
-            <hr class="mb-4">
+        </div>
+        <hr class="mb-4">
+        <div class="row">
             <div class="col-4">
                 <div class="card shadow p-1 mb-3 bg-white rounded">
                     <img class="img-fluid" src="https://picsum.photos/500?random=2" alt="" srcset="">
@@ -59,7 +63,8 @@
                     excepturi libero sapiente aliquam provident. Deserunt, officia!
                 </p>
                 <a name="" id="" class="btn btn-primary" href="{{ url('galeri') }}"
-                    role="button">Selengkapnya <i class="fa-solid fa-arrow-right-long"></i></a>
+                    role="button">Selengkapnya
+                    <i class="fa-solid fa-arrow-right-long"></i></a>
             </div>
         </div>
         <hr class="mb-4">
@@ -76,7 +81,8 @@
                     excepturi libero sapiente aliquam provident. Deserunt, officia!
                 </p>
                 <a name="" id="" class="btn btn-primary" href="{{ url('galeri') }}"
-                    role="button">Selengkapnya <i class="fa-solid fa-arrow-right-long"></i></a>
+                    role="button">Selengkapnya
+                    <i class="fa-solid fa-arrow-right-long"></i></a>
 
             </div>
             <div class="col-4">
